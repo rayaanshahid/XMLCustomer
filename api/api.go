19bdc5dee7aca19fa90520e749977927bb130491
rayaanshahid <rayaan.shahid.rs@gmail.com>
2019-07-04 02:04:00 -0700

    api finished

--- api/api.go
+++ api/api.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-func showPostHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func getClient(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	//setCors(w)
 	var tableclient database.Tableclient
 	database.DB.Where("Invoice_Point_ID = ?", ps.ByName("invoicePointId")).First(&tableclient)
@@ -18,11 +18,10 @@ func showPostHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 		http.Error(w, err.Error(), 500)
 		return
 	}
-
 	w.Write(res)
 }
 
-func indexPostHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func getClients(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	//setCors(w)
 	var tableclients []database.Tableclient
 	database.DB.Find(&tableclients)
@@ -40,8 +39,8 @@ func main() {
 	// add router and routes
 	router := httprouter.New()
 
-	router.GET("/clients/:invoicePointId", showPostHandler)
-	router.GET("/clients", indexPostHandler)
+	router.GET("/clients/:invoicePointId", getClient)
+	router.GET("/clients", getClients)
 
 	// add database
 	_, err := database.Init()
@@ -53,9 +52,20 @@ func main() {
 	log.Println("connected to DB")
 
 	clients := getClients()
-	testPost := Post{Author: "Dorper", Message: "GoDoRP is Dope"}
-	DB.Create(&testPost)
-
+	//You can insert multiple records too
+	for i:=0;i<len(clients.Clients);i++{
+			tableclient := database.Tableclient {Client_Name: clients.Clients[i].Client_Name, Invoice_Name:clients.Clients[i].Invoice_Name,
+				Invoice_Point_ID:clients.Clients[i].Invoice_Point_ID, Invoice_Add1:clients.Clients[i].Invoice_Add1,
+				Invoice_Add2:clients.Clients[i].Invoice_Add2, Invoice_Town:clients.Clients[i].Invoice_Town, Invoice_Country:clients.Clients[i].Invoice_Country,
+				Invoice_Postcode:clients.Clients[i].Invoice_Postcode, Invoice_Tel_No:clients.Clients[i].Invoice_Tel_No,
+				Invoice_Email:clients.Clients[i].Invoice_Email, Owning_Region:clients.Clients[i].Owning_Region}
+			DB := db.Create(&tableclient)
+			if DB.Error != nil {
+				log.Print("Row did not enter !!!")
+			}else {
+				log.Println("One row entered !!!")
+			}
+	}
 	// print env
 	env := os.Getenv("APP_ENV")
 	if env == "production" {
